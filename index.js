2c7ad9111edc4bb55cb8f06555e4fbf4ee7cdd3e
CARLOS PEREZ <karloseps08@gmail.com>
2023-09-08 21:21:49 -0600

    clone el repo de platzi y lo subi a mi github usando el comando git remote set-url origin <nueva_URL_del_repositorio>

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const options = {
 app.use(cors(options));
 
 app.get('/', (req, res) => {
-  res.send('Hola mi server en express');
+  res.send('Hola mi server en express ch4');
 });
 
 app.get('/nueva-ruta', (req, res) => {
