1767849b0f19c1d6006296864962e6aaa41039b7
CARLOS PEREZ <karloseps08@gmail.com>
2023-09-16 01:27:21 -0600

    instalacion de sequelize ORM

--- services/product.service.js
+++ services/product.service.js
@@ -1,16 +1,14 @@
 const { faker } = require('@faker-js/faker');
 const boom = require('@hapi/boom');
 
-const pool = require('../libs/postgres.pool');
+const sequelize = require('../libs/sequelize');
 
 class ProductsService {
 
   constructor(){
     this.products = [];
     this.generate();
-    this.pool = pool;
-    this.pool.on('error', (err)=> console.error(err));
-  }
+   }
 
   generate() {
     const limit = 100;
@@ -36,8 +34,8 @@ class ProductsService {
 
   async find() {
     const query = 'SELECT * FROM task';
-    const rta = await this.pool.query(query);
-    return rta.rows;
+    const [data] = await sequelize.query(query);
+    return data;
   }
 
   async findOne(id) {
