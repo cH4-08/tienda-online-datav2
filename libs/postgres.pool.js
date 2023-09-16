b183df84ca5e4a01b8d7efd43db587c9a9c4f33c
CARLOS PEREZ <karloseps08@gmail.com>
2023-09-16 01:00:59 -0600

    inyeccion con variables de entorno

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,13 +1,12 @@
 const { Pool } = require('pg');
 
+const { config } = require('./../config/config');
 
-  const pool = new Pool({
-    host: 'localhost',
-    port: 5432,
-    user: 'charles',
-    password: 'ch080',
-    database: 'my_store_ch'
-  });
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
+  const pool = new Pool({ connectionString: URI });
 
 
 module.exports = pool;
