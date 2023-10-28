ce2c033843610bd6fc9af486679b574b8bb47c65
Carlos Perez <karloseps08@gmail.com>
2023-10-27 22:11:58 -0600

    creacion de relaciones muchos a muchos

--- db/models/index.js
+++ db/models/index.js
@@ -3,6 +3,7 @@ const {Customer, CustomerSchema} = require ('./customer.model.js');
 const {Category, CategorySchema} = require ('./category.model.js');
 const {Product, ProductSchema} = require ('./product.model.js');
 const {Order, OrderSchema} = require ('./order.model.js');
+const {OrderProduct, OrderProductSchema} = require ('./order-product.model.js');
 
 
 function setupModels(sequelize){
@@ -11,12 +12,14 @@ function setupModels(sequelize){
     Category.init(CategorySchema, Category.config(sequelize));
     Product.init(ProductSchema, Product.config(sequelize));
     Order.init(OrderSchema, Order.config(sequelize));
+    OrderProduct.init(OrderProductSchema, OrderProduct.config(sequelize));
 
     User.associate(sequelize.models);
     Customer.associate(sequelize.models);
     Category.associate(sequelize.models);
     Product.associate(sequelize.models);
     Order.associate(sequelize.models);
+
 }
 
 module.exports = setupModels;
